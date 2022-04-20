982770acb4cce42328b88a92242a4ea1c8334cd6
jmsmith1018 <jmsmith1018@gmail.com>
2022-04-20 16:21:54 -0600

    feat: add `#show`, `#create`, and `#new` routes/actions for Event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,24 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index]
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :date, :creator_id)
   end
 end
