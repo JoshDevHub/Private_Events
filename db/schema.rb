7d1204634794ced467e4054c38ae5b72109040ae
jmsmith1018 <jmsmith1018@gmail.com>
2022-04-20 16:18:37 -0600

    feat: add association between events and their creating user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_20_201342) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_20_211237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_20_201342) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_20_201342) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users", column: "creator_id"
 end
