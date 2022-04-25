4feb3d10bf851a814de98bca5c116eeacbb2467b
jmsmith1018 <jmsmith1018@gmail.com>
2022-04-25 11:16:47 -0600

    feat: model and schema changes to support `EventAttendance`

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_21_163344) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_25_164820) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "event_attendances", id: false, force: :cascade do |t|
+    t.integer "attendee_id"
+    t.integer "attended_event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["attended_event_id"], name: "index_event_attendances_on_attended_event_id"
+    t.index ["attendee_id"], name: "index_event_attendances_on_attendee_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name"
