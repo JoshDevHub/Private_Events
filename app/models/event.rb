43b7a8649e02e5da58e5dd9a88a69a423d490531
jmsmith1018 <jmsmith1018@gmail.com>
2022-04-25 16:20:12 -0600

    feat: split user's event attendances into past/future events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
-
   has_many :event_attendances, foreign_key: :attended_event
-
   has_many :attendees, through: :event_attendances
+
+  scope :past, -> { where("date <= ?", Time.now) }
+  scope :future, -> { where("date > ?", Time.now) }
 end
