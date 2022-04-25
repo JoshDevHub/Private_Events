df95046ce832c582428c70c5c08bd5505a25b59c
jmsmith1018 <jmsmith1018@gmail.com>
2022-04-25 11:29:12 -0600

    feat: add controller and routes for `EventAttendances`

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -1,3 +1,23 @@
 class EventAttendancesController < ApplicationController
-  
+  before_action :authenticate_user!
+
+  def new
+    @event_attendance = EventAttendance.new
+  end
+
+  def create
+    @event_attendance = current_user.event_attendances.build(event_attendance_params)
+
+    if @event_attendance.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_attendance_params
+    params.require(:event_attendance).permit(:attended_event_id, :attendee_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :events, only: %i[new create index show]
   resources :users, only: :show
+  resources :event_attendances, only: %i[new create]
   # get "/users/:id", to: "users#show", as: show_user_path
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
