bad2a167ed175d794fd7280ce0d8ca2fddf09b4c
jmsmith1018 <jmsmith1018@gmail.com>
2022-05-02 10:07:44 -0600

    feat: add model validations to `User` and `Event`

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,12 @@ class Event < ApplicationRecord
   has_many :event_attendances, foreign_key: :attended_event
   has_many :attendees, through: :event_attendances
 
+  validates :name, presence: true
+  validates :creator, presence: true
+  validates :location, presence: true
+  validates :date, presence: true
+  validates :description, presence: true
+
   scope :past, -> { where("date <= ?", Time.now) }
   scope :future, -> { where("date > ?", Time.now) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events, foreign_key: "creator_id"
-
   has_many :event_attendances, foreign_key: :attendee_id
-
   has_many :attended_events, through: :event_attendances
+
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
 end
