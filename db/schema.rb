36491d8e9750aa0ec73380c7196812d1e4f239e9
jmsmith1018 <jmsmith1018@gmail.com>
2022-05-02 10:07:19 -0600

    feat: add `description` column to `Event` model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_25_164820) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_02_155411) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_25_164820) do
     t.datetime "updated_at", null: false
     t.bigint "creator_id"
     t.string "location"
+    t.string "description"
   end
 
   create_table "users", force: :cascade do |t|
