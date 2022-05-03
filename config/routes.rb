1bc20f9e33c556b058be2445aedc6dd1469cf026
jmsmith1018 <jmsmith1018@gmail.com>
2022-05-03 14:41:16 -0600

    style: organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :events
   resources :users, only: :show
-  resources :event_attendances, only: %i[new create]
-  # get "/users/:id", to: "users#show", as: show_user_path
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :events
+
+  resources :event_attendances, only: %i[new create]
+
   root "events#index"
 end
