9c08b2ec3137c0fa5d503bf82d18f1affe1fc0d8
jmsmith1018 <jmsmith1018@gmail.com>
2022-05-03 10:02:05 -0600

    feat: add routes/actions for editing `Events`

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :events, only: %i[new create index show]
+  resources :events
   resources :users, only: :show
   resources :event_attendances, only: %i[new create]
   # get "/users/:id", to: "users#show", as: show_user_path
