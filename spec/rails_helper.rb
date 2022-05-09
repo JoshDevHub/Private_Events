adece30cc1fe56633718b548f7d5082d9471a8cf
jmsmith1018 <jmsmith1018@gmail.com>
2022-05-09 11:51:25 -0600

    chore: configure test suite setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,12 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include Warden::Test::Helpers
+
+  config.include FactoryBot::Syntax::Methods
+
+  config.after :each do
+    Warden.test_reset!
+  end
 end
